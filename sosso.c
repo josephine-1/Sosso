ecb07538d867e3d74861cfbdf87b4d8d2a9cb15e
Josephine <sosodjibrildiop@gmail.com>
2022-08-02 12:02:49 +0000

    code-somme

---
+++ sosso.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a,b,S;
+    printf("Donner la valeur de a\n");
+    scanf("%d",&a);
+    printf("Donner la valeur de b\n");
+    scanf("%d",&b);
+    S=a+b;
+    printf("La somme est %d",S);
+    return 0;
+}
